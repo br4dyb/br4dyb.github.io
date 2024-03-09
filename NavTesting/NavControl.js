0468f4204fb0d852fba332ee6bb82ad8dc17f957
br4dyb <154681345+br4dyb@users.noreply.github.com>
2024-03-09 15:53:48 -0600

    chnges

--- NavTesting/NavControl.js
+++ NavTesting/NavControl.js
@@ -1,11 +1,29 @@
-const NavMenu = document.getElementById('NavList')
+const NavMenu = document.getElementById('NavList');
+const OpenNavMenuBttn = document.getElementById('OpenNavButton');
 
 function OpenNavMenu() {
 
    if(NavMenu.style.display == 'none') {
     NavMenu.style.display = 'Flex'
+    OpenNavMenuBttn.style.display = 'none'
    } else {
     NavMenu.style.display = 'none'
+    OpenNavMenuBttn.style.display = 'block'
    }
 
-}
+};
+
+const MainNavMenu = document.getElementById('MainNavLinksList');
+const MoreNavMenu = document.getElementById('MoreNavLinksList');
+
+function OpenMoreNavLinks() {
+
+   if(MainNavMenu.style.display == 'none') {
+      MainNavMenu.style.display = 'Block'
+      MoreNavMenu.style.display = 'none'
+     } else {
+      MainNavMenu.style.display = 'none'
+      MoreNavMenu.style.display = 'Block'
+     }
+
+};

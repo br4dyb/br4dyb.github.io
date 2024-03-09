2c3a7209d917123f991d1567661b4e8f91a1c9be
br4dyb <154681345+br4dyb@users.noreply.github.com>
2024-03-09 02:20:14 -0600

    Update page

---
+++ NavTesting/NavControl.js
@@ -0,0 +1,11 @@
+const NavMenu = document.getElementById('NavList')
+
+function OpenNavMenu() {
+
+   if(NavMenu.style.display == 'none') {
+    NavMenu.style.display = 'Flex'
+   } else {
+    NavMenu.style.display = 'none'
+   }
+
+}

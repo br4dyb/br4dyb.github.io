19bb577e96ab32a9256d868274da489c83b26506
br4dyb <halting.credo.00@icloud.com>
2024-03-11 23:35:05 -0500

    ~

--- Scripts/GitHub API/GitHub-API.js
+++ Scripts/GitHub API/GitHub-API.js
@@ -1,5 +1,3 @@
-const { Octokit } = require("@octokit/core");
-
 const octokit = new Octokit({
   auth: process.env.PAT_BR4DYB // Accessing GitHub Secret as an environment variable
 });
@@ -17,13 +15,46 @@ async function getGitHubPagesBuildStatus(owner, repo) {
       console.log("Latest GitHub Pages build status:", latestRun.status);
       console.log("Conclusion:", latestRun.conclusion);
       console.log("Details:", latestRun.html_url); // URL to the workflow run on GitHub
+
+      return latestRun.conclusion, latestRun.status;
+
     } else {
       console.log("No GitHub Pages workflow runs found.");
+
+      return "Error", "Could not Fetch!";
     }
   } catch (error) {
     console.error("Error fetching GitHub Pages build status:", error);
+
+    return "Error", "Could not Fetch!";
   }
 }
 
 // Replace 'owner' and 'repo' with your GitHub username and repository name
-getGitHubPagesBuildStatus("br4dyb", "br4dyb");
+// getGitHubPagesBuildStatus("br4dyb", "br4dyb");
+
+
+function CheckMainBuildStatus() {
+
+  console.log("Checking Build Status | Via API ")
+
+ let BuildSuccess, data = getGitHubPagesBuildStatus("br4dyb", "br4dyb");
+ let NewText = document.createElement('div');
+ 
+ if (BuildSuccess == "Error") {
+
+  NewText.innerHTML = `
+  <p> <b> Results: </b> </p>
+  <p> Conclusion: <b> ERROR! </b> </p>
+  <p> Status: <b> ERROR! </b> </p>
+`; } else {
+
+  NewText.innerHTML = `
+  <p> Results </p>
+  <p> Conclusion: ${BuildSuccess} </p>
+  <p> Status: ${data} </p>
+`;};
+ 
+document.body.appendChild(NewText)
+
+};

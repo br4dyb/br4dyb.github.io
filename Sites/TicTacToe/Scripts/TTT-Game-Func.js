933fb0f7b13c33c8362f182d4d47101eccfcc798
br4dyb <halting.credo.00@icloud.com>
2024-03-16 16:25:04 -0500

    ~

--- Sites/TicTacToe/Scripts/TTT-Game-Func.js
+++ Sites/TicTacToe/Scripts/TTT-Game-Func.js
@@ -2,15 +2,17 @@
 const GameGrid = document.getElementById('FullGameTable');
 const AllGameCells = GameGrid.querySelectorAll('td');
 
-let PlayerPiece = "X"
+let PlayerPiece = "X";
+
+let ComputerOwnedCells = [];
+let PlayerOwnedCells = [];
 
 
 function MakeAllCellsAvailable() {
     // Makes all cells available at game start:
     AllGameCells.forEach(element => {
         element.classList.toggle("AvailableCellArea");
-        console.log(`Added "AvailableCellArea" for: ` + element.nodeName);
-    });
+    }); 
 }
 
 function ComputerSelectCell() {
@@ -24,6 +26,14 @@ function ComputerSelectCell() {
             let RandomCellSelected = CurrentAvailableCells[randomIndex];
     
             RandomCellSelected.classList.remove('AvailableCellArea');
+
+            let cellId = RandomCellSelected.id;
+            let cellNumber = parseInt(cellId.split('_')[1]);
+
+            ComputerOwnedCells.push(cellNumber); // Add selected cell to ComputerOwnedCells
+
+            console.log(`[Computer]: has selected cell ${cellNumber}!`);
+            console.log(`[ComputerOwnedCells]: ${ComputerOwnedCells.toString()}`);
     
             RandomCellSelected.textContent = ComputerPiece;
             RandomCellSelected.style.background = "#FFBD33";
@@ -31,7 +41,7 @@ function ComputerSelectCell() {
             setTimeout(() => {
                 RandomCellSelected.style.background = 'unset';
             }, 500);
-            
+
         }else if(CurrentAvailableCells.length === 0) {
             GameEnd();
         }
@@ -42,21 +52,39 @@ function ComputerSelectCell() {
     }
 }
 
-let CoolDown = false
+let CoolDown = false;
 
 function CellSelected(GameCell) {
+
+    setTimeout(() => {
+        CoolDown = false;
+        console.log(`[CoolDown]: ${CoolDown.valueOf()}`);
+    }, 1200);
+
     // Check if selected cell is available:
     if(GameCell.classList.contains("AvailableCellArea") && CoolDown === false) {
-        console.log(GameCell + `has been clicked!`);
+
+        CoolDown = true;
+        console.warn(`[CoolDown]: ${CoolDown.valueOf()}`);
+
+        console.log(GameCell.id + ` has been clicked!`);
         GameCell.classList.toggle("AvailableCellArea");
         GameCell.textContent = PlayerPiece
 
+        let cellId = GameCell.id;
+        let cellNumber = parseInt(cellId.split('_')[1]);
+
+        PlayerOwnedCells.push(cellNumber); // Add selected cell to PlayerOwnedCells
+
+        console.log(`[Player]: Cell ${cellNumber} has been clicked!`);
+        console.log(`[PlayerOwnedCells]: ${PlayerOwnedCells.toString()}`);
+
         setTimeout(() => { 
             ComputerSelectCell() 
-        }, 1000)
+        }, 1000);
 
     } else {
-        GameCell.style.background = 'red'
+        GameCell.style.background = '#E06262'
         setTimeout(() => {
             GameCell.style.background = 'unset'
         }, 500);

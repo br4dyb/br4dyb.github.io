e305c19775c29c789a18a2991d8b7f387de30623
br4dyb <halting.credo.00@icloud.com>
2024-03-16 16:00:29 -0500

    ~

--- Sites/TicTacToe/Scripts/TTT-Game-Func.js
+++ Sites/TicTacToe/Scripts/TTT-Game-Func.js
@@ -13,12 +13,48 @@ function MakeAllCellsAvailable() {
     });
 }
 
+function ComputerSelectCell() {
+    if(PlayerPiece === "X") {
+        let ComputerPiece = "O";
+        let CurrentAvailableCells = GameGrid.querySelectorAll('td.AvailableCellArea');
+
+        
+        if(CurrentAvailableCells.length >= 1) {
+            let randomIndex = Math.floor(Math.random() * CurrentAvailableCells.length);
+            let RandomCellSelected = CurrentAvailableCells[randomIndex];
+    
+            RandomCellSelected.classList.remove('AvailableCellArea');
+    
+            RandomCellSelected.textContent = ComputerPiece;
+            RandomCellSelected.style.background = "#FFBD33";
+    
+            setTimeout(() => {
+                RandomCellSelected.style.background = 'unset';
+            }, 500);
+            
+        }else if(CurrentAvailableCells.length === 0) {
+            GameEnd();
+        }
+
+    } else if(PlayerPiece === "O"){
+        let ComputerPiece = "X";
+
+    }
+}
+
+let CoolDown = false
+
 function CellSelected(GameCell) {
     // Check if selected cell is available:
-    if(GameCell.classList.contains("AvailableCellArea")) {
-        console.log(GameCell + `has been clicked! / function ran!`);
+    if(GameCell.classList.contains("AvailableCellArea") && CoolDown === false) {
+        console.log(GameCell + `has been clicked!`);
         GameCell.classList.toggle("AvailableCellArea");
         GameCell.textContent = PlayerPiece
+
+        setTimeout(() => { 
+            ComputerSelectCell() 
+        }, 1000)
+
     } else {
         GameCell.style.background = 'red'
         setTimeout(() => {

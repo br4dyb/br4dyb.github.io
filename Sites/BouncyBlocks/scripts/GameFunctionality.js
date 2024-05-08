5b3293618ef5ba6e0c1ccc47ce7a8683604816ac
br4dyb <halting.credo.00@icloud.com>
2024-05-08 18:40:15 -0500

    ~

--- Sites/BouncyBlocks/scripts/GameFunctionality.js
+++ Sites/BouncyBlocks/scripts/GameFunctionality.js
@@ -198,7 +198,7 @@ function CreateObsticles() {
     ObsticleWrap.style.top = '-10px'
     ObsticleWrap.style.left = '100%'
 
-    ObsticleWrap.style.animation = `move-obsticle forwards ${ObsticleMoveSpeed} ease-out`
+    ObsticleWrap.style.animation = `move-obsticle forwards ${ObsticleMoveSpeed}`
 
         FullGameplayArea.appendChild(ObsticleWrap);
 

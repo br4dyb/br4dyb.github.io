fc468bec7293f4ce78be93a0c252f46c1796ca81
br4dyb <halting.credo.00@icloud.com>
2024-10-27 19:01:51 -0500

    ~

--- V2/Scripts/Search.js
+++ V2/Scripts/Search.js
@@ -3,16 +3,45 @@ const LandingSearchBar = document.getElementById('LandingSearchInput');
 const LandingSearchAutoCompleteWrap = document.getElementById('SearchInputAutoComplete');
 
 // Variables:
-let SearchableTags = ['games', 'fun', 'example'];
+let SearchableTags = ['example'];
+let AvaialbleSearchTags = [];
+let SearchableTagFound = false;
+        
+
+// Functions:
 function LandingSearchAutoComplete(){
     let LandingSearchValue = LandingSearchBar.value;
-    console.log('Input Registered');
+    SearchableTagFound = false;
     
     if(LandingSearchValue == ''){
-        console.info('Search Empty');
+        // console.info('Search Empty');
         LandingSearchAutoCompleteWrap.style.display = 'none';
     }else{
-        console.log(LandingSearchValue);
+        // Check Search for AutoComplete Tags:
         LandingSearchAutoCompleteWrap.style.display = 'flex';
+        LandingSearchAutoCompleteWrap.innerHTML = '';
+
+        let FilteredSearchValue = LandingSearchValue.toLowerCase().trim();
+
+        let SearchedArray = FilteredSearchValue.split(' ');
+
+        SearchedArray.forEach(SearchedTag => {
+            SearchableTags.forEach(SearchableTag => {
+                SearchableTag = SearchableTag.toLowerCase().trim();
+                if(SearchableTag.includes(SearchedTag)){
+                    let NewSearchableTagElm = document.createElement('p');
+                    NewSearchableTagElm.classList.add('AutoCompleteOption');
+                    NewSearchableTagElm.innerText = SearchableTag;
+                    LandingSearchAutoCompleteWrap.appendChild(NewSearchableTagElm);
+                    SearchableTagFound = true;
+                }
+            });
+        });
     }
+
+    // Hide AutoComplete if no Tag Found:
+    if(!SearchableTagFound){
+        LandingSearchAutoCompleteWrap.style.display = 'none';
+    }
+
 }

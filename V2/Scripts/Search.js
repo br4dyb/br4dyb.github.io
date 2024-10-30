71c2124816fd4d17c59b862ce6b35193982e0cc4
br4dyb <halting.credo.00@icloud.com>
2024-10-29 21:51:32 -0500

    ~

--- V2/Scripts/Search.js
+++ V2/Scripts/Search.js
@@ -3,7 +3,7 @@ const LandingSearchBar = document.getElementById('LandingSearchInput');
 const LandingSearchAutoCompleteWrap = document.getElementById('LandingSearchInputAutoComplete');
 const SearchSitesSearchBar = document.getElementById('SearchSitesInput');
 const SearchSitesAutoCompleteWrap = document.getElementById('SearchSitesInputAutoComplete');
-
+SearchSitesGird = SearchSitesGrid // Already Defined
 
 
 let CurrentSearchInput;
@@ -12,18 +12,7 @@ let CurrentAutoCompleteWrap;
 // Variables:
 let SearchableTags = [];
 let AvaialbleSearchTags = [];
-let SearchableTagFound = false;
-
-// Custom Tags:
-let CustomTags = [
-    {TagNames: ['My Account', 'Account'], Link: 'OpenMyAccount()'},
-];
-CustomTags.forEach(Tag => {
-    Tag.TagNames.forEach(CustomTagName => {
-        SearchableTags.push(CustomTagName)
-    })
-})
-        
+let SearchableTagFound = false;    
 
 // Functions:
 function SearchAutoComplete(inputElement){
@@ -56,6 +45,18 @@ function SearchAutoComplete(inputElement){
         let FilteredSearchValue = SearchValue.toLowerCase().trim();
         let SearchedArray = FilteredSearchValue.split(' ');
 
+        // Add Current Input as Option:
+        let NewSearchableTagElm = document.createElement('p');
+        NewSearchableTagElm.classList.add('AutoCompleteOption');
+        let PrettyFormatOption = String(SearchValue);
+        let FirstChar = PrettyFormatOption.charAt(0)
+        let FirstCharUpper = FirstChar.toUpperCase()
+        PrettyFormatOption = PrettyFormatOption.replace(FirstChar, FirstCharUpper)
+        NewSearchableTagElm.innerText = PrettyFormatOption;
+        NewSearchableTagElm.onclick= () => SubmitSiteSearch(SearchValue)
+        CurrentAutoCompleteWrap.appendChild(NewSearchableTagElm);
+        
+
         // Check each word for SearchableTags:
         SearchedArray.forEach(SearchedTag => {
             SearchableTags.forEach(SearchableTag => {
@@ -67,6 +68,7 @@ function SearchAutoComplete(inputElement){
                     let NewSearchableTagElm = document.createElement('p');
                     NewSearchableTagElm.classList.add('AutoCompleteOption');
                     NewSearchableTagElm.innerText = SearchableTag;
+                    NewSearchableTagElm.onclick= () => SubmitSiteSearch(SearchableTag)
         
                     // Check for Duplicates:
                     if(!AvaialbleSearchTags.includes(SearchableTag)){
@@ -82,9 +84,54 @@ function SearchAutoComplete(inputElement){
     }
 
     // Hide AutoComplete if no Tag Found:
-    if(!SearchableTagFound){
-        // Set Results to NOT Found:
-        CurrentAutoCompleteWrap.style.display = 'none';
+    // if(!SearchableTagFound){
+    //     // Set Results to NOT Found:
+    //     CurrentAutoCompleteWrap.style.display = 'none';
+    // }
+
+}
+
+function SubmitSiteSearch(SearchValue){
+    if(CurrentView != SearchSitesWrap){
+        SwitchPage('SearchSites');
     }
 
+    // Update Search Input Elements:
+    SearchSitesSearchBar.value = SearchValue;
+    SearchSitesAutoCompleteWrap.style.display = 'none';
+    LandingSearchBar.value = '';
+    LandingSearchAutoCompleteWrap.style.display = 'none';
+
+    console.info('Searching for:');
+    console.log(SearchValue);
+
+    // Perform Search:
+
+    let ResultFound = false;
+    let SearchableSites = SearchSitesGrid.querySelectorAll('.SiteCell');
+    let SearchableResources = SearchSitesGrid.querySelectorAll('.ExtraResources');
+
+    // Check Each Site:
+    SearchableSites.forEach(Site => {
+        Site.classList.add('hidden'); // Initally Hide until Searched for
+        
+        let SiteTags = String(Site.dataset.SiteTags)
+        SiteTags = SiteTags.split(',') // Convert to Array
+
+        // Check Each Tag:
+        SiteTags.forEach(Tag => {
+            // Format Strings:
+            Tag = String(Tag.trim());
+            Tag = Tag.toLowerCase();
+            SearchValue = SearchValue.trim();
+            SearchValue = SearchValue.toLowerCase();
+
+            if(Tag.includes(SearchValue)){
+                // Result Found:
+                ResultFound = true;
+                Site.classList.remove('hidden')
+            }
+        })
+
+    });
 }

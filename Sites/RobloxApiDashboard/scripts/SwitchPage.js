2900876181bbe37764e22cead8038c875ed732b2
br4dyb <halting.credo.00@icloud.com>
2025-04-17 19:11:19 -0500

    ~

---
+++ Sites/RobloxApiDashboard/scripts/SwitchPage.js
@@ -0,0 +1,56 @@
+// Page Wraps:
+let HomepageWrap = document.getElementById('homepageContent');
+let AccountWrap = document.getElementById('myAccountFullWrap');
+let ChangeLogWrap = document.getElementById('changeLogFullWrap');
+let ContactUsWrap = document.getElementById('contactUsFullWrap');
+
+// Variables:
+let HomepageNavButton = document.getElementById('HomepageNavButton');
+let CurrentView = HomepageWrap;
+
+// Function(s):
+function SwitchPage(NewPage){
+    // Assign NewPage
+
+    if(NewPage == 'Account'){
+        NewPage = AccountWrap;
+    }
+
+    if(NewPage == 'ChangeLog'){
+        NewPage = ChangeLogWrap;
+    }
+
+    if(NewPage == 'ContactUs'){
+        NewPage = ContactUsWrap;
+    }
+
+    // Manage Homepage Button Visibility:
+    if(NewPage == 'Homepage'){
+        NewPage = HomepageWrap;
+        HomepageNavButton.classList.add('hidden');
+    }else{
+        HomepageNavButton.classList.remove('hidden');
+    }
+
+    // Chek for Aready Viewing this Page:
+    if(CurrentView === NewPage){
+        return
+    }else{
+    // Reset Scrollbar Position:
+        document.documentElement.scrollTop = 0;  // For modern browsers
+        document.body.scrollTop = 0;  // For older browsers
+
+    // Animate in NewPage:
+    CurrentView.style.opacity = 0;
+        setTimeout(() => { 
+            CurrentView.classList.add('hidden') 
+            NewPage.style.opacity = 0;
+            NewPage.classList.remove('hidden')
+            CurrentView = NewPage;
+            setTimeout(() => {
+                NewPage.style.opacity = 1;
+            }, 50);
+        }, 400);
+
+    }
+}
